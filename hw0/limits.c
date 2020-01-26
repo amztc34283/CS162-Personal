d58c5b129266fb6ed93b0597f65be21764148a9f
Billy - Vagrant <chunheichau@berkeley.edu>
2020-01-26 19:25:00 +0000

    finish limit

--- hw0/limits.c
+++ hw0/limits.c
@@ -3,8 +3,12 @@
 
 int main() {
     struct rlimit lim;
-    printf("stack size: %ld\n", 0L);
-    printf("process limit: %ld\n", 0L);
-    printf("max file descriptors: %ld\n", 0L);
+    int resources[] = {RLIMIT_NOFILE, RLIMIT_NPROC, RLIMIT_STACK};
+    getrlimit(RLIMIT_STACK, &lim);
+    printf("stack size: %ld\n", lim.rlim_cur);
+    getrlimit(RLIMIT_NPROC, &lim);
+    printf("process limit: %ld\n", lim.rlim_cur);
+    getrlimit(RLIMIT_NOFILE, &lim);
+    printf("max file descriptors: %ld\n", lim.rlim_cur);
     return 0;
 }
