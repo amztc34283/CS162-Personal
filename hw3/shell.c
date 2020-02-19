e659a35dac10eb48e13ed9dc73b0a94f09e1ee03
Billy - Vagrant <chunheichau@berkeley.edu>
2020-02-19 07:26:56 +0000

    Finished adding basic functionality into the shell.

--- hw3/shell.c
+++ hw3/shell.c
@@ -30,6 +30,8 @@ pid_t shell_pgid;
 
 int cmd_exit(struct tokens *tokens);
 int cmd_help(struct tokens *tokens);
+int cmd_pwd(struct tokens *tokens);
+int cmd_cd(struct tokens *tokens);
 
 /* Built-in command functions take token array (see parse.h) and return int */
 typedef int cmd_fun_t(struct tokens *tokens);
@@ -44,6 +46,8 @@ typedef struct fun_desc {
 fun_desc_t cmd_table[] = {
   {cmd_help, "?", "show this help menu"},
   {cmd_exit, "exit", "exit the command shell"},
+  {cmd_pwd, "pwd", "print current working directory"},
+  {cmd_cd, "cd", "change directory to the destination"},
 };
 
 /* Prints a helpful description for the given command */
@@ -56,6 +60,24 @@ int cmd_help(unused struct tokens *tokens) {
 /* Exits this shell */
 int cmd_exit(unused struct tokens *tokens) {
   exit(0);
+}
+
+/* Prints current working directory */
+int cmd_pwd(unused struct tokens *tokens) {
+  char *result = getcwd(NULL, 0);
+  if (result == NULL)
+    return -1;
+  printf("%s\n", result);
+  free(result);
+  return 1;
+}
+
+/* Changes directory */
+int cmd_cd(struct tokens *tokens) {
+  char *result = tokens_get_token(tokens, 1);
+  if (chdir(result) == -1)
+    return -1;
+  return 1;
 }
 
 /* Looks up the built-in command, if it exists. */
